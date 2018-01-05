7d99af0440698d0f8193fbb690eaf0421e3e6d43
aliadil <aliadilg@gmail.com>
2018-01-05 23:48:33 +0500

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,6 +25,21 @@ public class PlayerController : MonoBehaviour {
 	}
     void Jump()
     {
-        rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        if (IsGrounded())
+        {
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+    public LayerMask groundLayer;
+    bool IsGrounded()
+    {
+        if (Physics2D.Raycast(this.transform.position, Vector2.down, 0.2f, groundLayer.value))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
