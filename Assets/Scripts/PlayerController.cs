ae571b9e5ace4b2d491df0b7535518df13f867e3
aliadil <aliadilg@gmail.com>
2018-01-05 23:26:47 +0500

    PlayerController Changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public float jumpForce = 6f;
+    public Rigidbody2D rigidBody;
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +17,14 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        // this is exaclty same as if(Input.GetMouseButtonDown(0)	==	true)
+        if (Input.GetMouseButtonDown(0))
+        {
+            Jump();
+        }
 	}
+    void Jump()
+    {
+        rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
 }
