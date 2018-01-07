c12359f02eb4a2c3266476bc85055b1bbf78ff09
aliadil <aliadilg@gmail.com>
2018-01-07 20:00:54 +0500

    GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+        
+    //called to start the game
+    public void StartGame()
+    {
+
+    }
+
+    //called when player die
+    public void GameOver()
+    {
+
+    }
+
+    //called when player decide to go back to the menu 
+    public void BackToMenu()
+    {
+
+    }
+}
