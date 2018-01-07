8f2a8896dd1a726903d556b0ddae205f4d7caad3
aliadil <aliadilg@gmail.com>
2018-01-07 20:27:25 +0500

    GameManager update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,23 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
-        
+public enum GameState
+{
+    menu,
+    inGame,
+    gameOver
+}
+
+public class GameManager : MonoBehaviour
+{
+    public GameState currentGameState = GameState.menu;
+
+    void Start()
+    {
+        StartGame();
+    }
     //called to start the game
     public void StartGame()
     {
-
+        SetGameState(GameState.inGame);
     }
 
     //called when player die
     public void GameOver()
     {
-
+        SetGameState(GameState.gameOver);
     }
 
     //called when player decide to go back to the menu 
     public void BackToMenu()
     {
+        SetGameState(GameState.menu);
+    }
 
+    void SetGameState(GameState newGameState)
+    {
+        if (newGameState == GameState.menu)
+        {
+            //setup Unity scene for menu state
+        }
+        else if (newGameState == GameState.inGame)
+        {
+            // setup unity scene for inGame state
+        }
+        else if (newGameState == GameState.gameOver)
+        {
+            // setup unity scene for gameOver state
+        }
     }
 }
+
