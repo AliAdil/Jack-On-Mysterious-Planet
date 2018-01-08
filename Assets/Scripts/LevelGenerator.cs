ca138855e21d19a99b14ae503f7debe062924ab7
aliadil <aliadilg@gmail.com>
2018-01-09 03:13:44 +0500

    Level Generator variable initialization

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+	// Use this for initialization
+    public static LevelGenerator instance;
+    public List<LevelPiece> levelPrefabs = new List<LevelPiece>();
+    public Transform levelStartPoint;
+    public List<LevelPiece> pieces = new List<LevelPiece>();
+
+    void Awake()
+    {
+        instance = this;
+    }
+}
