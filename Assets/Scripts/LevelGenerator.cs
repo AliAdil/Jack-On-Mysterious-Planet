963cd8851af91113d7a2b471f08930e4b306134c
aliadil <aliadilg@gmail.com>
2018-01-09 03:37:08 +0500

    importent comments added to script

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -6,8 +6,11 @@ public class LevelGenerator : MonoBehaviour {
 
 	// Use this for initialization
     public static LevelGenerator instance;
+    //all level pieces blueprints used to copy from
     public List<LevelPiece> levelPrefabs = new List<LevelPiece>();
+    // starting point of the very first level piece
     public Transform levelStartPoint;
+    // all level piece that are currently in the level
     public List<LevelPiece> pieces = new List<LevelPiece>();
 
     void Awake()
