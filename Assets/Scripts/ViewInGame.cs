35d70cd7f0fa1f6d6b9bf5e858808e69c2e74cbb
aliadil <aliadilg@gmail.com>
2018-01-11 17:44:13 +0500

    Ingame View Script for UI updates

---
+++ Assets/Scripts/ViewInGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewInGame : MonoBehaviour {
+
+    public Text coinLabel;
+    public Text scoreLabel;
+    public Text highscoreLabbel;
+
+    void Update()
+    {
+        if(GameManager.instance.currentGameState == GameState.inGame){
+            scoreLabel.text = PlayerController.instance.GetDistance().ToString("f0");
+            coinLabel.text = GameManager.instance.collectedCoins.ToString();
+            highscoreLabbel.text = PlayerPrefs.GetFloat("highscore", 0).ToString("f0");
+            
+
+        }
+    }
+}
